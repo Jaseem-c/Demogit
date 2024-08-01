4e4826e37c8a91ea1282ebf5b2c1cc2ebd8aa8e6
Jaseem-c <seamjaseem@gmail.com>
2024-08-01 20:17:53 +0530

    start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Footer from './Components/Footer'
+import Heder from './Components/Heder'
+
+function App() {
+
+
+  return (
+    <>
+     <Heder/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
